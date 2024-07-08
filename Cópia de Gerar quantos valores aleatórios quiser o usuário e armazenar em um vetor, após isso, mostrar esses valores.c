5e5214436e38c988c2210e75feb9f74384f3677f
mrrobot378 <165595620+mrrobot378@users.noreply.github.com>
2024-07-08 12:29:48 -0300

    Add files via upload

---
+++ Cópia de Gerar quantos valores aleatórios quiser o usuário e armazenar em um vetor, após isso, mostrar esses valores.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main () {
+	setlocale (LC_ALL,"Portuguese");
+	
+     srand(time(0));
+     
+     int valores[50];
+     int numero;
+	 int qtd;
+     
+     printf ("Insira a quantidade de valores aleátorios que deseja:\n");
+     scanf ("%d", &qtd);
+     
+     for (int i = 0; i < qtd; i++)
+	 {
+	 numero = rand () %25;
+	 valores[i] = numero;
+     
+     }
+     printf ("\nOs valores aleatórios são:\n");
+     for (int i = 0; i < qtd; i++)
+     {
+     	printf ("%d\n", valores[i]);
+	 }
+	return 0;
+}
