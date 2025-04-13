46b25c19f01e7894facab47ca1eb9be8b046f119
Lucas Gregorio <165595620+mrrobot378@users.noreply.github.com>
2025-04-13 07:30:51 -0300

    Add files via upload

---
+++ funcao_loop_1_10.c
@@ -0,0 +1,9 @@
+ /* Programa para imprimir de 1 até 10 lado a lado
+    na mesma linha com três espaços entre eles */
+    void imprimir_ao_lado()
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            printf("%d   ",i);
+        }
+    }
